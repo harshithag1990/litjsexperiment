f8cab49db9a7e8719f21a914aa582372283fad06
harshithag1990 <harshithag@novaworks.ai>
2025-11-03 17:03:29 +0530

    created a basic lit component

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/my-element.ts',
+      name: 'MyElement',
+      fileName: (format) => `my-element.${format}.js`,
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['lit'],
+      output: {
+        globals: {
+          lit: 'lit'
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+});
